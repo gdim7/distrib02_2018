0702a99246fa55fab6bbc9c8a5f7ac0cf63d4bc7
canagno123 <xristosanagnostou95@hotmail.com>
2018-03-04 15:40:12 +0200

    Minor changes

--- client.py
+++ client.py
@@ -2,14 +2,18 @@
 
 import socket
 
-
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = ""
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+print "Please provide your IP address, your preffered UDP port and username divided by a space"
+MESSAGE = raw_input()
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+print "received data:", data
 while 1:
 	MESSAGE = raw_input()
 	s.send(MESSAGE)
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import socket
 import select
 import sys
@@ -10,44 +9,69 @@ import Queue
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
-GROUPS = {'group1' : ['asndj', 'sjdhjas'], 'group2' : ['asdsad', 'sdhahsj']}
-#MEMBERS = {}
-
+GROUPS = {}
+USERS = {}
+USER_READY_STR = 'A new user has now been created. Your user id is: '
+usrid = 0
+register = {}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setblocking(0)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-inputs = [ s ]
-outputs = [ ]
+inputs = [s]
+outputs = []
 
 message_queue = {}
 
 while inputs:
 	readable, writeable, exceptional = select.select(inputs, outputs, inputs)
+	print 'Sanity'
 	for r in readable:
 		if r is s:
 			conn, addr = r.accept()
 			conn.setblocking(0)
 			inputs.append(conn)
 			print 'Connection address:', addr
-
 			message_queue[conn] = Queue.Queue()
+			register[conn] = True
 		else:
 			data = r.recv(BUFFER_SIZE)
 			if data:
-				if (data == '!lg'):
+				if (register[r] == True):
+					register[r] == False
+					ip = data.split(' ')[0]
+					udp_port = data.split(' ')[1]
+					username = data.split(' ')[2]
+					USERS[usrid] = []
+					USERS[usrid].append(ip)
+					USERS[usrid].append(udp_port)
+					USERS[usrid].append(username)
+					message_queue[r].put(USER_READY_STR + str(usrid))
+					usrid += 1
+				elif (data == '!lg'):
 					grps = ''
 					for key in GROUPS:
 						grps += key + ' '
 					message_queue[r].put(grps)
+				
 				elif (data.startswith('!lm ')):
 					grp = data.split(' ')[1]
 					mbrs = ''
 					try:
 						for members in GROUPS[grp]:
 							mbrs += members + ' '
+					except:
+						mbrs = 'There is no such group.'
+					message_queue[r].put(mbrs)
+				elif (data.startswith('!j ')):
+					grp = data.split(' ')[1]
+					mbrs = ''
+					try:
+						if not grp in GROUPS:
+							GROUPS[grp] = []
+							GROUPS[grp].append()
 					except:
 						mbrs = 'There is no such group.'
 					message_queue[r].put(mbrs)
