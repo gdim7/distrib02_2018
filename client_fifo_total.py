f6d4c4db467a0a395034ae29af742733f093ed03
George Dimitrakopoulos <gdim95@gmail.com>
2018-03-12 14:33:46 +0200

    Add Total ordering

---
+++ client_fifo_total.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+
+import socket
+import select
+import sys
+import time
+from chat_utils import *
+import Queue
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+print 'Please provide your IP address, your preffered UDP port and username divided by a space'
+MESSAGE = raw_input()
+while (not len(MESSAGE.split(' ')) == 3):
+	print 'Usage is: <IP_ADDR> <PORT> <USERNAME>'
+	MESSAGE = raw_input()
+
+UDP_IP = MESSAGE.split(' ')[0]
+UDP_PORT = int(MESSAGE.split(' ')[1])
+USERNAME = MESSAGE.split(' ')[2]
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_sock.bind((UDP_IP, UDP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+usrid = str(data.split(' ')[4])
+print '[TRACKER]: ', data
+s.close()
+
+is_sequencer = {}
+timestamps = {}
+grp_timestamps = {}
+stored_msgs = {}
+grp_stored_msgs = {}
+seq_addr = {}
+seq_number = {}
+stored_msgs_seq = []
+
+def fifo_total_seq(msg):
+	MESSAGE = msg.split('|')[0]
+	current_grp = MESSAGE.split(' ')[1]
+	id_timestamp = msg.split('|')[1]
+	tid = id_timestamp.split(' ')[0]
+	t = int(id_timestamp.split(' ')[1])
+	if (tid == usrid):
+		correct_t = grp_timestamps[current_grp][tid]
+	else:
+		correct_t = grp_timestamps[current_grp][tid] + 1
+	if (correct_t == t):
+		if (not tid == usrid):
+			grp_timestamps[current_grp][tid] += 1
+		for (ip, port) in seq_addr[current_grp]:
+			udp_addr = (ip, int(port))
+			msg = 'PRINT_MSG$$$' + MESSAGE + '$$$' + str(seq_number[current_grp])
+			sent = udp_sock.sendto(msg, udp_addr)
+		seq_number[current_grp] += 1	
+		sys.stdout.write('\r')
+		sys.stdout.flush()
+		print MESSAGE
+		correct_t += 1
+		if (tid in grp_stored_msgs[current_grp]):
+			flag = True	
+			while(flag):
+				flag = False
+				for temp in grp_stored_msgs[current_grp][tid]:
+					if (correct_t == int(temp.split('|')[0])):
+						if (not tid == usrid):
+							grp_timestamps[current_grp][tid] += 1
+						correct_t += 1
+						for (ip, port) in seq_addr[current_grp]:
+							udp_addr = (ip, int(port))
+							msg = 'PRINT_MSG$$$' + temp.split('|')[1] + '$$$' + str(seq_number[current_grp])
+							sent = udp_sock.sendto(msg, udp_addr)
+						seq_number[current_grp] += 1	
+						sys.stdout.write('\r')
+						sys.stdout.flush()
+						print temp.split('|')[1]
+						grp_stored_msgs[current_grp][tid].remove(temp)
+						flag = True					
+	elif (correct_t < t):
+		for_store = str(t) + '|' + MESSAGE
+		if not tid in stored_msgs:
+			grp_stored_msgs[current_grp][tid] = []
+			grp_stored_msgs[current_grp][tid].append(for_store)
+		else:
+			grp_stored_msgs[current_grp][tid].append(for_store)		
+	else:
+		sys.stdout.write('\r')
+		sys.stdout.flush()			
+		return
+	return
+
+def fifo_total(msg):
+	MESSAGE = msg.split('$$$')[1]
+	grp = MESSAGE.split(' ')[1]
+	seq_no = int(msg.split('$$$')[2])
+	if (seq_no == seq_number[grp]):
+		seq_number[grp] += 1
+		sys.stdout.write('\r')
+		sys.stdout.flush()
+		print MESSAGE
+		if (len(stored_msgs_seq) > 0):
+			flag = True	
+			while(flag):
+				flag = False
+				for temp in stored_msgs_seq:
+					if (seq_number[grp] == int(temp.split('|')[0])):
+						seq_number[grp] += 1
+						sys.stdout.write('\r')
+						sys.stdout.flush()
+						print temp.split('|')[1]
+						stored_msgs_seq.remove(temp)
+						flag = True					
+	elif (seq_no > seq_number[grp]):
+		for_store = str(seq_number[grp]) + '|' + MESSAGE
+		stored_msgs_seq.append(for_store)		
+	else:
+		sys.stdout.write('\r')
+		sys.stdout.flush()			
+		return
+	return
+
+
+while 1:
+
+	sys.stdout.write('[' + USERNAME + ']> ')
+	sys.stdout.flush()
+
+	sockets_list = [sys.stdin, udp_sock]
+	outputs = []
+
+	read_sockets, write_sockets, error_sockets = select.select(sockets_list, outputs, [])
+
+	for socks in read_sockets:
+		if socks == udp_sock:	
+			MESSAGE, server = udp_sock.recvfrom(BUFFER_SIZE)
+			if MESSAGE.startswith('REMOVE_USER'):
+				removed_ip = MESSAGE.split(' ')[1]
+				removed_port = MESSAGE.split(' ')[2]
+				removed_id = MESSAGE.split(' ')[3]
+				for key in grp_timestamps:
+					for a in grp_timestamps[key]:
+						if a==removed_id:
+							del grp_timestamps[key][a]
+							break
+				for key in seq_addr:
+					if is_sequencer[key]:
+						for a in seq_addr[key]:
+							if a==(removed_ip, removed_port):
+								seq_addr[key].remove(a)
+								break		
+				for a in group_users:
+					if a == (removed_ip, removed_port):
+						group_users.remove((removed_ip, removed_port))
+				sys.stdout.write('\r')
+				sys.stdout.flush()
+			elif MESSAGE.startswith('U_R_SEQUENCER'):
+				if len(MESSAGE.split(' ')) == 2:
+					seq_grp = MESSAGE.split(' ')[1]
+					is_sequencer[seq_grp] = True
+					seq_addr[grp] = []
+					seq_number[seq_grp] = 0
+				elif len(MESSAGE.split(' ')) == 4:
+					if not grp in seq_addr:
+						seq_addr[grp] = []
+						seq_number[grp] = 0
+						seq_grp = MESSAGE.split(' ')[1]
+						for (ip, port) in group_users:
+							udp_addr = (ip, int(port))
+							sent = udp_sock.sendto('CURRENT_SEQ_NUMBER 0 ' + seq_grp, udp_addr)
+					new_addr = (MESSAGE.split(' ')[2], MESSAGE.split(' ')[3])
+					seq_addr[seq_grp].append(new_addr)
+				elif len(MESSAGE.split(' ')) == 5:
+					if not grp in seq_addr:
+						seq_addr[grp] = []
+						seq_number[grp] = 0
+					seq_grp = MESSAGE.split(' ')[1]
+					if is_sequencer[seq_grp]:
+						for (ip, port) in group_users:
+							udp_addr = (ip, int(port))
+							sent = udp_sock.sendto('CURRENT_SEQ_NUMBER 0 ' + seq_grp, udp_addr)	
+				sys.stdout.write('\r')
+				sys.stdout.flush()		
+			elif MESSAGE.startswith('MY_TIMESTAMP'):
+				new_id = MESSAGE.split(' ')[1]
+				new_grp = MESSAGE.split(' ')[3]
+				grp_timestamps[new_grp][new_id] = int(MESSAGE.split(' ')[2])
+				sys.stdout.write('\r')
+				sys.stdout.flush()	
+			elif MESSAGE.startswith('TIMESTAMP'):
+				new_id = MESSAGE.split(' ')[1]
+				new_grp = MESSAGE.split(' ')[4]
+				grp_timestamps[new_grp][new_id] = 0
+				udp_addr = (MESSAGE.split(' ')[2], int(MESSAGE.split(' ')[3]))
+				tmstmp_msg = 'MY_TIMESTAMP ' + usrid + ' ' + str(grp_timestamps[new_grp][usrid]) + ' ' + new_grp
+				sent = udp_sock.sendto(tmstmp_msg, udp_addr)
+				sys.stdout.write('\r')
+				sys.stdout.flush()
+			elif MESSAGE.startswith('NEW_USER'):
+				new_ip = MESSAGE.split(' ')[1]
+				new_port = MESSAGE.split(' ')[2]
+				new_id = MESSAGE.split(' ')[3]
+				updated_grp = MESSAGE.split(' ')[8]
+				if current_grp == updated_grp:
+					group_users.append((new_ip, new_port))
+				if is_sequencer[updated_grp]:
+					udp_addr = (new_ip, int(new_port))
+					sent = udp_sock.sendto('CURRENT_SEQ_NUMBER ' + str(seq_number[updated_grp]) + ' ' + updated_grp, udp_addr)
+				sys.stdout.write('\r')
+				sys.stdout.flush()
+				print MESSAGE.split(':')[1]
+			elif MESSAGE.startswith('CURRENT_SEQ_NUMBER'):
+				seq_number[MESSAGE.split(' ')[2]] = int(MESSAGE.split(' ')[1])
+				print MESSAGE
+				sys.stdout.write('\r')
+				sys.stdout.flush()
+			elif MESSAGE.startswith('PRINT_MSG'):
+				fifo_total(MESSAGE)
+			else:
+				my_group = MESSAGE.split(' ')[1]
+				if is_sequencer[my_group]:
+					fifo_total_seq(MESSAGE)
+				sys.stdout.write('\r')
+				sys.stdout.flush()
+		else:
+			MESSAGE = raw_input()
+			if (MESSAGE.startswith('!')):
+				try:
+					s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					s.connect((TCP_IP, TCP_PORT))
+				except:
+					print 'You are already connected with id: ', usrid
+				MESSAGE = 'ID: ' + usrid + ' ' + MESSAGE	
+				s.send(MESSAGE)
+				if (MESSAGE.split(' ')[2] == '!q'):
+					print 'Disconnecting...'
+					sys.exit()
+				if (MESSAGE.split(' ')[2] == '!j'):
+					grp = MESSAGE.split(' ')[3]
+					if not grp in grp_timestamps:
+						grp_timestamps[grp] = {}
+						grp_timestamps[grp][usrid] = 0
+						grp_stored_msgs[grp] = {}	
+					if not grp in is_sequencer:
+						is_sequencer[grp] = False	
+				if (MESSAGE.split(' ')[2] == '!e' and current_grp == MESSAGE.split(' ')[3]):
+					current_grp = None
+				data = s.recv(BUFFER_SIZE)
+				if (MESSAGE.split(' ')[2] == '!e' and data.startswith('You have been disconnected')):
+					try:
+						if is_sequencer[MESSAGE.split(' ')[3]]:
+							is_sequencer[MESSAGE.split(' ')[3]] = False
+							seq_addr[MESSAGE.split(' ')[3]] = []
+						del grp_timestamps[MESSAGE.split(' ')[3]]	
+					except:
+						pass		
+				if (MESSAGE.split(' ')[2] == '!w' and len(data.split(':')) == 2 and not data.startswith('Usage is:')):
+					current_grp = MESSAGE.split(' ')[3]
+					data = data.split(':')[1]
+					users_info  = data.splitlines()
+					group_users = []
+					for strng in users_info:
+						ip_port = strng.split(' ')
+						group_users.append((ip_port[0], ip_port[1]))
+				elif (MESSAGE.split(' ')[2] == '!j' and len(data.split(':')) == 2):
+					is_sequencer[MESSAGE.split(' ')[3]] = True
+				else:
+					print '[TRACKER]: ', data
+			else:
+				if (not current_grp == None):
+					grp_timestamps[current_grp][usrid] += 1
+					for user in group_users:
+						udp_addr = (user[0], int(user[1]))
+						send_msg = ''
+						send_msg = 'In ' + current_grp + ' ' + USERNAME + ' says:: ' + MESSAGE + '|' + usrid + ' ' + str(grp_timestamps[current_grp][usrid])
+						sent = udp_sock.sendto(send_msg, udp_addr)
+				else:
+					print 'Please join a group in order to send messages.'
---
+++ server_fifo_total.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+
+import socket
+import select
+import sys
+import Queue
+from chat_utils import *
+
+#ping_clients()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setblocking(0)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+sequencers = {}
+inputs = [s]
+outputs = []
+
+while inputs:
+	readable, writeable, exceptional = select.select(inputs, outputs, inputs)
+	for r in readable:
+		if r is s:
+			conn, addr = r.accept()
+			conn.setblocking(0)
+			inputs.append(conn)
+			print 'Connection address:', addr
+			message_queue[conn] = Queue.Queue()
+		else:
+			data = r.recv(BUFFER_SIZE)
+			if data:
+				if (not data.startswith('ID: ')):
+					ip = data.split(' ')[0]
+					udp_port = data.split(' ')[1]
+					username = data.split(' ')[2]
+					tempid = str(usrid)
+					USERS[tempid] = []
+					USERS[tempid].append(ip)
+					USERS[tempid].append(udp_port)
+					USERS[tempid].append(username)
+					message_queue[r].put(USER_READY_STR + tempid)
+					usrid += 1
+				elif (data.split(' ')[2] == '!lg'):
+					grps = 'groups: '
+					for key in GROUPS:
+						grps += '[' + key + '] '
+					message_queue[r].put(grps)
+				
+				elif (data.split(' ')[2] == '!lm'):
+					input_length = len(data.split(' '))
+					if (input_length == 4):
+						grp = data.split(' ')[3]
+						mbrs = 'members: '
+						try:
+							for members in GROUPS[grp]:
+								mbrs += '(' + members + ') '
+						except:
+							mbrs = NOGROUPERROR
+						message_queue[r].put(mbrs)
+					else:
+						message_queue[r].put(LMINPUTERROR)	
+				elif (data.split(' ')[2] == '!j'):
+					input_length = len(data.split(' '))
+					if (input_length == 4):
+						tempid = data.split(' ')[1]
+						grp = data.split(' ')[3]
+						if not grp in GROUPS:
+							GROUPS[grp] = []
+							GROUPS[grp] = [USERS[tempid][2]]
+							GROUPS_IDS[grp]= [tempid]
+							sequencers[grp] = tempid
+							message_queue[r].put('You have been connected to the group ' + grp)
+							udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+							udp_addr = (USERS[tempid][0], int(USERS[tempid][1]))
+							seq_msg = 'U_R_SEQUENCER ' + grp
+							sent = udp_socket.sendto(seq_msg, udp_addr)
+						else:
+							if not tempid in GROUPS_IDS[grp]:
+								if sequencers[grp] == None:
+									sequencers[grp] = tempid
+								GROUPS[grp].append(USERS[tempid][2])
+								GROUPS_IDS[grp].append(tempid)	
+								new_udp_ip = USERS[tempid][0]
+								new_udp_port = USERS[tempid][1]
+								new_username = USERS[tempid][2]
+								update_msg =  'NEW_USER ' + new_udp_ip + ' ' + new_udp_port + ' ' + tempid + ' :' + new_username + ' has now joined ' + grp
+								udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+								udp_addr = (USERS[sequencers[grp]][0], int(USERS[sequencers[grp]][1]))
+								seq_msg = 'U_R_SEQUENCER ' + grp + ' ' + USERS[tempid][0] + ' ' + USERS[tempid][1]
+								sent = udp_socket.sendto(seq_msg, udp_addr)
+								for user in GROUPS_IDS[grp]:
+									if not user == tempid:
+										udp_addr = (USERS[user][0], int(USERS[user][1]))
+										sent = udp_socket.sendto(update_msg, udp_addr)
+										new_timestamp_msg = 'TIMESTAMP ' + str(tempid) + ' ' + new_udp_ip + ' ' + new_udp_port + ' ' + grp
+										sent = udp_socket.sendto(new_timestamp_msg, udp_addr)		
+								message_queue[r].put('You have been connected to the group ' + grp)
+							else:
+								message_queue[r].put(JMEMBERERROR)
+					else:
+						message_queue[r].put(JINPUTERROR)
+				elif (data.split(' ')[2] == '!q'):
+					tempid = data.split(' ')[1]
+					for a in GROUPS.itervalues():
+						try:
+							a.remove(USERS[tempid][2])
+						except ValueError:
+							pass
+					for a in GROUPS_IDS.itervalues():
+						try:
+							a.remove(tempid)
+						except ValueError:
+							pass
+					seq_list = []
+					for a in sequencers:
+						try:
+							if tempid in sequencers[a]:
+								seq_list.append(a)
+						except ValueError:
+							pass
+					for grp in seq_list:
+						grp_empty = True
+						for a in GROUPS_IDS[grp]:
+									grp_empty = False
+									break
+						if tempid in sequencers[grp]:
+							sequencers[grp] = None
+						if  grp_empty:
+							del GROUPS[grp]
+							del GROUPS_IDS[grp]
+					remove_ip = USERS[tempid][0]
+					remove_port = USERS[tempid][1]
+					remove_msg = 'REMOVE_USER ' + remove_ip + ' ' + remove_port + ' ' + tempid
+					udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+					for user in USERS:
+						udp_addr = (USERS[user][0], int(USERS[user][1]))
+						sent = udp_socket.sendto(remove_msg, udp_addr)	
+					for grp in seq_list:
+						flag_seq = True
+						seq_addr = ('','')	
+						if (tempid == sequencers[grp]):
+							for user in GROUPS_IDS[grp]:
+								if flag_seq:
+									udp_addr = (USERS[user][0], int(USERS[user][1]))
+									flag_seq = False
+									sequencers[grp] = user
+									seq_addr = udp_addr
+									seq_msg = 'U_R_SEQUENCER ' + grp
+									sent = udp_socket.sendto(seq_msg, udp_addr)
+									break
+							if not seq_addr == ('',''):
+								for user in GROUPS_IDS[grp]:
+									if not user == sequencers[grp]:
+										seq_msg = 'U_R_SEQUENCER ' + grp + ' ' + USERS[user][0] + ' ' + USERS[user][1] + ' q'
+										sent = udp_socket.sendto(seq_msg, seq_addr) 		
+				elif (data.split(' ')[2] == '!w'):
+					input_length = len(data.split(' '))
+					if (input_length == 4):
+						tempid = data.split(' ')[1]
+						grp = data.split(' ')[3]
+						if not grp in GROUPS:
+							message_queue[r].put(NOGROUPERROR)
+						elif not tempid in GROUPS_IDS[grp]:
+							message_queue[r].put(NOUSERERROR)
+						else:
+							ips_ports  =  'answer:'
+							for temp_i in GROUPS_IDS[grp]:
+								ips_ports += USERS[temp_i][0] +  ' ' + USERS[temp_i][1] + '\n'
+							message_queue[r].put(ips_ports)
+					else:
+						message_queue[r].put(WINPUTERROR)	
+				elif (data.split(' ')[2] == '!e'):
+					input_length = len(data.split(' '))
+					if (input_length == 4):
+						tempid = data.split(' ')[1]
+						grp = data.split(' ')[3]
+						if not grp in GROUPS:
+							message_queue[r].put(NOGROUPERROR)
+						else:
+							flag = False
+							for a in GROUPS:
+								try:
+									if a == grp:
+										GROUPS[a].remove(USERS[tempid][2])
+										flag = True
+										break
+								except ValueError:
+									pass
+							for a in GROUPS_IDS:
+								try:
+									if a == grp:
+										GROUPS_IDS[a].remove(tempid)
+										break
+								except ValueError:
+									pass
+							grp_empty = True
+							for a in GROUPS_IDS[grp]:
+								grp_empty = False
+								break
+							if tempid in sequencers[grp]:
+								sequencers[grp] = None
+							if grp_empty:	
+								del GROUPS[grp]
+								del GROUPS_IDS[grp]
+							if (flag):			
+								message_queue[r].put('You have been disconnected from the group ' + grp)
+								if not grp_empty:
+									remove_ip = USERS[tempid][0]
+									remove_port = USERS[tempid][1]
+									remove_msg = 'REMOVE_USER ' + remove_ip + ' ' + remove_port + ' ' + tempid
+									print remove_msg
+									udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+									flag_seq = True
+									if not tempid == sequencers[grp]:
+										flag_seq = False
+									seq_addr = ('','')
+									for user in GROUPS_IDS[grp]:
+										udp_addr = (USERS[user][0], int(USERS[user][1]))
+										sent = udp_socket.sendto(remove_msg, udp_addr)
+										if flag_seq:
+											flag_seq = False
+											sequencers[grp] = user
+											seq_addr = udp_addr
+											seq_msg = 'U_R_SEQUENCER ' + grp
+											sent = udp_socket.sendto(seq_msg, udp_addr)
+									if not seq_addr == ('',''):
+										for user in GROUPS_IDS[grp]:
+											print user, sequencers
+											if not user == sequencers[grp]:
+												seq_msg = 'U_R_SEQUENCER ' + grp + ' ' + USERS[user][0] + ' ' + USERS[user][1] + ' q'
+												sent = udp_socket.sendto(seq_msg, seq_addr)
+							else:
+								message_queue[r].put(NOUSERERROR)	
+					else:
+						message_queue[r].put(EINPUTERROR)
+				else:
+					message_queue[r].put(CMDERROR)
+				print 'received data:', data
+				if r not in outputs:
+					outputs.append(r)
+			else:
+				if r in outputs:
+					outputs.remove(r)
+				inputs.remove(r)
+				r.close()
+				del message_queue[r]
+
+	for w in writeable:
+		try:
+			msg = message_queue[w].get_nowait()
+		except:
+			if w in outputs:
+				outputs.remove(w)
+		else:
+			w.send(msg)
+			inputs.remove(w)
+			if w in outputs:
+				outputs.remove(w)
+			w.close()
+			del message_queue[w]
+
+	for e in exceptional:
+		inputs.remove(e)
+		if e in outputs:
+			outputs.remove(e)
+		e.close()
+		del message_queue[e]
